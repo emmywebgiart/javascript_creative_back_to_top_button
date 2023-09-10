fabf27050747a1fb718f48eab71559bc768b2b7b
Emmanuel-Nowaii <eam@nowaii.com>
2023-09-10 00:34:35 -0600

    Feat: Modificaciones generales, cambio de titulo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let buttonBack = document.getElementById("button_back")
 
 document.addEventListener('scroll', () => {
-    if(window.scrollY > 300) {
+    if (window.scrollY > 300) {
         buttonBack.classList.add('show_button_back')
     } else {
         buttonBack.classList.remove('show_button_back')
